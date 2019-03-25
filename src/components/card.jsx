5d75e8ef8252e6d4f7542b4aa746ae1ae4b31d07
user <user@users-iMac.local>
2019-03-25 22:35:17 +0000

    2 cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -19,30 +19,59 @@ export default class card extends Component {
   }
 
   render() {
-    const { cardName, Apr, BalanceTransferOfferDuration, PurchaseOfferDurationm, creditAvailable } = this.props.cards[0];
     const {annualincome } = this.props.applicant;
-    
     
     const cardwidth = {
       width: '18rem',
     };
+
+    const anytimecard = (
+        <div className="card" style={cardwidth}>
+          {/* <img src="..." className="card-img-top"alt="..."/> */}
+          <div className="card-body">
+            <h5 className="card-title">{this.props.cards[0].cardName}</h5>
+            <p className="card-text">£{this.props.cards[0].creditAvailable}</p>
+            <p className="card-text">{this.props.cards[0].Apr}</p>
+            <p className="card-text">{this.props.cards[0].BalanceTransferOfferDuration}</p>
+            <p className="card-text">{this.props.cards[0].PurchaseOfferDuration}</p>
+            <button className="btn btn-primary" onClick={(e) => this.chooseCard(parseInt(this.props.cards[0].creditAvailable))}>Choose</button>
+            <button className="btn btn-danger" onClick={(e) => this.removeCard(parseInt(this.props.cards[0].creditAvailable))}> Remove</button>
+          </div>
+        </div>)
+
+    const studentcard = (
+        <div className="card" style={cardwidth}>
+          {/* <img src="..." className="card-img-top"alt="..."/> */}
+          <div className="card-body">
+            <h5 className="card-title">{this.props.cards[1].cardName}</h5>
+            <p className="card-text">£{this.props.cards[1].creditAvailable}</p>
+            <p className="card-text">{this.props.cards[1].Apr}</p>
+            <p className="card-text">{this.props.cards[1].BalanceTransferOfferDuration}</p>
+            <p className="card-text">{this.props.cards[1].PurchaseOfferDuration}</p>
+            <button className="btn btn-primary" onClick={(e) => this.chooseCard(parseInt(this.props.cards[1].creditAvailable))}>Choose</button>
+            <button className="btn btn-danger" onClick={(e) => this.removeCard(parseInt(this.props.cards[1].creditAvailable))}> Remove</button>
+          </div>
+        </div>)
+
+    const liquidcard = (
+      <div className="card" style={cardwidth}>
+    {/* <img src="..." className="card-img-top"alt="..."/> */}
+    <div className="card-body">
+      <h5 className="card-title">{this.props.cards[2].cardName}</h5>
+      <p className="card-text">£{this.props.cards[2].creditAvailable}</p>
+      <p className="card-text">{this.props.cards[2].Apr}</p>
+      <p className="card-text">{this.props.cards[2].BalanceTransferOfferDuration}</p>
+      <p className="card-text">{this.props.cards[2].PurchaseOfferDuration}</p>
+      <button className="btn btn-primary" onClick={(e) => this.chooseCard(parseInt(this.props.cards[2].creditAvailable))}>Choose</button>
+      <button className="btn btn-danger" onClick={(e) => this.removeCard(parseInt(this.props.cards[2].creditAvailable))}> Remove</button>
+    </div>
+  </div>)
+
+
     return (
       <div>
-        { parseInt(annualincome) > 16000 ? <p>Over <strong>16000</strong>, you are allowed Liquid card and anywhere card</p> : <p>under 16000 - you can have anywhere card</p> }
+        { parseInt(annualincome) > 16000 ? (<div className="row">{liquidcard} <br/> {anytimecard} </div>) : anytimecard }
         <p>Your Total credit is {this.state.totalCredit}</p>
-        <div className="card" style={cardwidth}>
-            {/* <img src="..." className="card-img-top"alt="..."/> */}
-            <div className="card-body">
-              <h5 className="card-title">{cardName}</h5>
-              <p className="card-text">£{creditAvailable}</p>
-              <p className="card-text">{Apr}</p>
-              <p className="card-text">{BalanceTransferOfferDuration}</p>
-              <p className="card-text">{PurchaseOfferDurationm}</p>
-              <button className="btn btn-primary" onClick={(e) => this.chooseCard(parseInt(creditAvailable))}>Choose</button>
-              <button className="btn btn-danger" onClick={(e) => this.removeCard(parseInt(creditAvailable))}> Remove</button>
-            </div>
-          </div>
-
       </div>
     )
   }
